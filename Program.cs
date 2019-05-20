64d92f9896d1250a0e4c808d670186e095aa8555
Admin <Admin@DESKTOP-89NTJEN>
2019-05-21 10:36:11 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelsestatement
+{
+    class Program
+    {
+        // Viewing the student great using if else condition
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Percentage:");
+            float percentage = float.Parse(Console.ReadLine());
+
+            if (percentage >=80)
+                Console.WriteLine("Grade A");
+            else if (percentage >= 70)
+                Console.WriteLine("Grade B");
+            else if (percentage >= 60)
+                Console.WriteLine("Grade C");
+            else
+                Console.WriteLine("Grade D");
+        }
+    } 
+}
+   
